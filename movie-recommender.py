5ed13377be8293900364a796124ef5d413f7439d
mdepasquale <depasquale@esteco.com>
2020-05-08 10:46:53 +0200

    change variable names and change script name

--- movie-recommender.py
+++ movie-recommender.py
@@ -2,12 +2,12 @@ import numpy as np
 from lightfm.datasets import fetch_movielens
 from lightfm import LightFM
 
-def sample_recommendation(model, train_data, labels, user_ids, n_known=3, n_results=2):
+def sample_recommendation(model, train_data, item_labels, user_ids, n_known=3, n_results=2):
     n_users, n_items = train_data.shape
     for user_id in user_ids:
-        known_positives = labels[train_data.tocsr()[user_id].indices]
+        known_positives = item_labels[train_data.tocsr()[user_id].indices]
         scores = model.predict(user_id, np.arange(n_items))
-        top_items = labels[np.argsort(-scores)]
+        top_items = item_labels[np.argsort(-scores)]
         top_items_filtered = [item for item in top_items if item not in known_positives]
         print("User %s" % user_id)
         print("    Known positives:")
@@ -17,18 +17,18 @@ def sample_recommendation(model, train_data, labels, user_ids, n_known=3, n_resu
         for x in top_items_filtered[:n_results]:
             print("        %s" % x)
 
-# fetch items
+# fetch items with positive ratings (account for positive feedback only)
 data = fetch_movielens(min_rating = 4.0)
 # take train data and test data
 train_set = data['train']
 test_set = data['test']
 # take labels for all items
-labels = data['item_labels']
+item_labels = data['item_labels']
 # show data
 print(repr(train_set))
 print(repr(test_set))
 
-# define model
+# define model (hybrid method)
 model = LightFM(loss = 'warp',
                 random_state=2016,
                 no_components=100,
@@ -40,4 +40,4 @@ model.fit(train_set, epochs=30, num_threads=2)
 users_ids_list = [3, 25, 451, 737, 901]
 known_items_to_show = 5
 recommendations_to_show = 3
-sample_recommendation(model=model, train_data=train_set, labels=labels, user_ids=users_ids_list, n_known=known_items_to_show, n_results=recommendations_to_show)
+sample_recommendation(model=model, train_data=train_set, item_labels=item_labels, user_ids=users_ids_list, n_known=known_items_to_show, n_results=recommendations_to_show)
