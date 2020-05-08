ee40c0459d220d836ce891d8292b5006bdcc981d
mdepasquale <depasquale@esteco.com>
2020-05-08 16:44:32 +0200

    ready for model training of book rs

--- book-recommender.py
+++ book-recommender.py
@@ -36,7 +36,7 @@ print(books_metadata.shape)
 print('Select only some fields from books metadata dataset')
 books_metadata_selected = books_metadata[['book_id', 'average_rating', 'is_ebook', 'num_pages', 
                                           'publication_year', 'ratings_count', 'language_code']]
-print(books_metadata_selected.sample(2))
+print(books_metadata_selected.sample(2).to_string(index=False))
 
 print('Perform some manipulation on data...')
 # using pandas cut method to convert fields into discrete intervals
@@ -60,27 +60,27 @@ books_metadata_selected['language_code'].replace('it-IT', 'ita', inplace=True)
 # convert is_ebook column into 1/0 where true=1 and false=0
 books_metadata_selected['is_ebook'] = books_metadata_selected.is_ebook.map(lambda x: 1*(x == 'true'))
 print('Books metadata after some manipulation')
-print(books_metadata_selected.sample(5))
+print(books_metadata_selected.sample(5).to_string(index=False))
 
 ############################################################################################################################### work on interactions data
 print('\nInteractions data:')
 print('Fields')
 print(interactions.columns.values)
 print('Some samples from dataset')
-print(interactions.sample(2))
+print(interactions.sample(2).to_string(index=False))
 print('Shape of interactions data')
 print(interactions.shape)
 
 # Limit the interactions data to selected fields
 print('Select only some fields from interactions dataset')
 interactions_selected = interactions[['user_id', 'book_id', 'is_read', 'rating']]
-print(interactions_selected.sample(2))
+print(interactions_selected.sample(2).to_string(index=False))
 
 print('Perform some manipulation on data...')
 # convert is_read column into 1/0 where True=1 and False=0
 interactions_selected['is_read'] = interactions_selected.is_read.map(lambda x: 1*(x == True))
 print('Interaction data after some manipulation')
-print(interactions_selected.sample(5))
+print(interactions_selected.sample(5).to_string(index=False))
 
 # Since we have two fields denoting interaction between a user and a book, is_read and rating
 # let's see how many data points we have where the user hasn't read the book but have given the ratings.
@@ -91,3 +91,56 @@ print(interactions_counts.to_string())
 # From the above results, we can conclusively infer that users with ratings >= 1 have all read the book.
 # Therefore, we'll use the ratings as the final score, drop interactions where is_read is false,
 # and limit interactions from random 500 users to limit the data size for further analysis
+print('\nSelect only user-item interactions with users that have actually read the book, and a limited number (to simplify calculations)')
+interactions_selected = interactions_selected.loc[interactions_selected['is_read']==1, ['user_id', 'book_id', 'rating']]
+interactions_selected = interactions_selected[interactions_selected['user_id'].isin(random.sample(list(interactions_selected['user_id'].unique()), k=5000))]
+print(interactions_selected.sample(10).to_string(index=False))
+print('Final interactions dataset shape')
+print(interactions_selected.shape)
+
+############################################################################################################################### data processing
+
+# Now, let's transform the available data into CSR sparse matrix that can be used for matrix operations.
+# We will start by the process by creating books_metadata matrix which is np.float64 csr_matrix of shape ([n_books, n_books_features])
+# Each row contains that book's weights over features. However, before we create a sparse matrix, we'll first create a item dictionary for future references
+
+item_dict ={}
+df = books_metadata[['book_id', 'title']].sort_values('book_id').reset_index()
+
+for i in range(df.shape[0]):
+    item_dict[(df.loc[i,'book_id'])] = df.loc[i,'title']
+    
+# dummify categorical features
+books_metadata_selected_transformed = pd.get_dummies(books_metadata_selected, columns = ['average_rating', 'is_ebook', 'num_pages', 
+                                                                                         'publication_year', 'ratings_count', 
+                                                                                         'language_code'])
+
+books_metadata_selected_transformed = books_metadata_selected_transformed.sort_values('book_id').reset_index().drop('index', axis=1)
+print('First rows of books metadata transformed')
+print(books_metadata_selected_transformed.head(5))
+
+# convert to csr matrix
+books_metadata_csr = csr_matrix(books_metadata_selected_transformed.drop('book_id', axis=1).values)
+print('Sparse matrix of books metadata')
+print(books_metadata_csr)
+
+# Next we'll create an iteractions matrix which is np.float64 csr_matrix of shape ([n_users, n_books]).
+# We'll also create a user dictionary for future use cases
+user_book_interaction = pd.pivot_table(interactions_selected, index='user_id', columns='book_id', values='rating')
+# fill missing values with 0
+user_book_interaction = user_book_interaction.fillna(0)
+print('First rows of interactions data transformed')
+print(user_book_interaction.head(5))
+
+user_id = list(user_book_interaction.index)
+user_dict = {}
+counter = 0 
+for i in user_id:
+    user_dict[i] = counter
+    counter += 1
+
+# convert to csr matrix
+user_book_interaction_csr = csr_matrix(user_book_interaction.values)
+print('Sparse matrix of interactions data')
+print(user_book_interaction_csr)
+
